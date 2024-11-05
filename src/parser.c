7861ccdbcef6e7944c2d10a782e79e0c97664d94
tndreka <tndreka@student.42heilbronn.de>
2024-11-05 18:47:54 +0100

    on the process finishing the table

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: tndreka <tndreka@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 15:09:14 by tndreka           #+#    #+#             */
-/*   Updated: 2024/11/05 14:35:11 by tndreka          ###   ########.fr       */
+/*   Updated: 2024/11/05 18:47:05 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,18 @@ t_lexer *check_prompt(char *prompt, t_msh *msh);
 
 t_lexer *tokenize_prompt(char *prompt, t_msh *msh);
 
+int pass_to_table(t_lexer **token_list, t_msh *msh, t_table **table);
 
 void minishell_parser(char *prompt, t_msh *msh)
 {
     t_lexer     *token_list = NULL;
-    t_lexer     *headof_list;
+    //t_lexer     *headof_list;
     t_table     *table = NULL;
-    (void)msh;
+    int         i;
 
+    i = 0;
     token_list = check_prompt(prompt, msh);
-    headof_list = token_list;
+    //headof_list = token_list;
     //token_list = lexer(prompt);
     if(NULL == token_list)
         return;
@@ -43,7 +45,7 @@ void minishell_parser(char *prompt, t_msh *msh)
         i = pass_to_table(&token_list, msh, &table);
         if (-1 == i)
         {
-            free_everything(msh, headof_list, table);
+            //free_everything(msh, headof_list, table);
             break;
         }
     }
@@ -67,7 +69,9 @@ int pass_to_table(t_lexer **token_list, t_msh *msh, t_table **table)
 {
     t_lexer     *token;
     t_cmd       *new_command;
-       
+
+    (void)msh;
+    (void)table;
     token = (*token_list);
     new_command = malloc(sizeof(t_cmd));
     if (NULL == new_command)
@@ -85,4 +89,57 @@ int pass_to_table(t_lexer **token_list, t_msh *msh, t_table **table)
             return -1;
         }
     }
+    else if (token->type == REDIRIN)
+    {
+        new_command->content = ft_strdup(token->data);
+        if (NULL == new_command)
+        {
+            free(new_command);
+            return -1;
+        }
+    }
+    else if (token->type == REDIROUT)
+    {
+        new_command->content = ft_strdup(token->data);
+        if (NULL == new_command)
+        {
+            free(new_command);
+            return -1;
+        }
+    }
+    else if (token->type == HEREDOC)
+    {
+        new_command->content = ft_strdup(token->data);
+        if (NULL == new_command)
+        {
+            free(new_command);
+            return -1;
+        }
+    }
+    else if (token->type == APPEND)
+    {
+        new_command->content = ft_strdup(token->data);
+        if (NULL == new_command)
+        {
+            free(new_command);
+            return -1;
+        }
+    }
+    else if (token->type == STRING)
+    {
+        new_command->content = ft_strdup(token->data);
+        if(NULL == new_command)
+        {
+            free(new_command);
+            return -1;
+        }       
+    }
+    else if (token->type == PIPE)
+    {
+        if(NULL == new_command)
+    
+            return -1;
+    
+    }
+    return 0;
 }
